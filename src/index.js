f71b241284c464bd88946c0d0e2c82f6a03101c3
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-04-27 12:24:29 -0500

    set up base style

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
+import './sass/main.scss';
 
 ReactDOM.render(
   <React.StrictMode>
