10090736cba899733f64de41010cb68c17c6e40e
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-04-28 13:54:49 -0500

    route all pages in the App component and include the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import HomePage from './pages/homepage';
+import SignIn from './pages/sign-in';
+import SignUp from './pages/sign-up';
+import NotFound from './pages/not-found';
+import Header from './layout/header';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="app">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/*" component={NotFound} />
+      </Switch>
     </div>
   );
 }
