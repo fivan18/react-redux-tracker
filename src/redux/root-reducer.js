cd04b67ecf53b8cc809f17abf9abb4d5300b7e0a
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-04-28 09:17:59 -0500

    set up redux react session

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { sessionReducer } from 'redux-react-session';
 
 import fooReducer from './foo/foo.reducer';
 
 const rootReducer = combineReducers({
   foo: fooReducer,
+  session: sessionReducer,
 });
 
 export default rootReducer;
