c855bb62a2141db45225984852a31a47f414eeda
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-05-06 16:07:26 -0500

    add openRoutineDay action for session

--- src/redux/session/session.actions.js
+++ src/redux/session/session.actions.js
@@ -55,3 +55,57 @@ export const logout = (history) => () => sessionService.loadSession()
     // eslint-disable-next-line
               console.log(err);
   });
+
+export const openRoutineDay = (day, history) => () => sessionService.loadSession()
+  .then(({ token }) => {
+    axios({
+      method: 'post',
+      url: 'http://localhost:3000/routines',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        data: {
+          attributes: {
+            day: day.toISOString().substring(0, 10),
+          },
+        },
+      },
+    })
+      .then(({ data: { data: { id } } }) => {
+        history.push(`/routine/${id}`);
+      })
+      .catch(() => {
+        axios({
+          method: 'get',
+          url: 'http://localhost:3000/routines',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          data: {
+            data: {
+              attributes: {
+                day: day.toISOString().substring(0, 10),
+              },
+            },
+          },
+        })
+          .then(({ data: { data: routines } }) => {
+            const routine = routines
+              .find((routine) => routine.attributes.day === day.toISOString()
+                .substring(0, 10));
+
+            if (routine) {
+              history.push(`/routine/${routine.id}`);
+            } else {
+              history.push('/not-found');
+            }
+          })
+          .catch(() => {
+            history.push('/not-found');
+          });
+      });
+  })
+  .catch(() => {
+    history.push('signin');
+  });
