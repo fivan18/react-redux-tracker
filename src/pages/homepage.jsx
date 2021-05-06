0c521ceb4f6092b9353327c9aa09d90e799d56da
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-05-06 16:08:23 -0500

    dispatch openRoutineDay from daypicker and home pages

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const HomePage = () => (
-  <div className="home-page">
-    Home Page
-  </div>
-);
+import { openRoutineDay } from '../redux/session/session.actions';
 
-export default HomePage;
+const Homepage = ({
+  history, openRoutineDay,
+}) => {
+  useEffect(() => {
+    let mounted = true;
+    if (mounted) {
+      const day = new Date(Date.now());
+      openRoutineDay(day, history);
+    }
+
+    // eslint-disable-next-line no-return-assign
+    return () => mounted = false;
+  }, []);
+
+  return (
+    <div />
+  );
+};
+
+const { shape, func } = PropTypes;
+
+Homepage.propTypes = {
+  history: shape({
+    push: func,
+  }).isRequired,
+  openRoutineDay: func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  openRoutineDay: (day, history) => dispatch(openRoutineDay(day, history)),
+});
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(withRouter(Homepage));
